d696290a0018118406117afdd4ace1dc18549f05
Gavin Loughridge <gavinlough@gmail.com>
2018-01-11 13:50:09 -0600

    created basic funcitons

---
+++ posts/posts.component.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict'
+
+  angular.module('app')
+    .component('posts', {
+      controller: controller,
+      templateUrl: '/posts/posts.html'
+    })
+
+  function controller($firebaseArray, $firebaseAuth) {
+    const vm = this
+
+    vm.$onInit = function () {
+      vm.selectedPost = '😀'
+      var auth = $firebaseAuth()
+
+      auth.$signInAnonymously().then(function(authData) {
+        var ref = firebase.database().ref().child('posts')
+
+        vm.posts = $firebaseArray(ref)
+      }).catch(function(error) {
+        console.error("Authentication failed:", error);
+      });
+    }
+
+    vm.addPost = function() {
+      vm.posts.$add({
+        text: vm.selectedPost,
+        views: 0,
+        created: new Date().getTime()
+      });
+    };
+
+    vm.viewPost = function(e, post) {
+      e.preventDefault()
+      console.log(post);
+      let update = vm.posts.$getRecord(post.$id)
+      update.views += 1
+      vm.posts.$save(update)
+    }
+  }
+}());
